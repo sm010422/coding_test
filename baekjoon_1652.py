9d96bcaa9df74c52d957333b3ad8c9129325d2a2
Park Sang Min <88922405+sm010422@users.noreply.github.com>
2024-07-25 00:29:44 +0900

    git head practice - reset practice

--- baekjoon_1652.py
+++ baekjoon_1652.py
@@ -6,3 +6,5 @@ print("this is master branch and this is branch master branch practice")
 
 
 print("this is master branch and we will commit one more time")
+
+print("and this si git reset practice")
