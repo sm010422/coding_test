f9e658c3ee460b8d293f721ef76f7f1ec89be13c
Park Sang Min <sm010422@naver.com>
2024-08-20 17:25:24 +0900

    programmers coding test level1 (대충만든 자판)

---
+++ programmers/lessons_160586.py
@@ -0,0 +1,74 @@
+function solution(keymap, targets) {
+    
+    
+    function make_arr(array,where_push)
+    {
+        for(let i = 0; i < array.length; i++)
+            {
+                where_push.push([]);
+                for(let j = 0; j < array[i].length; j++)
+                    {
+                        where_push[i].push(array[i][j]);
+                    }
+            }
+    }
+    
+    const map = [];
+    const targets_array = [];
+    
+    make_arr(keymap,map);
+    make_arr(targets,targets_array);
+    
+    const result = []
+    
+    function find_target(targets_arr)
+    {
+        let the_smallest= [];
+        let return_value = 0;
+
+        for(let i = 0; i < map.length; i++)
+            {
+                for(let j = 0; j < map[i].length; j++)
+                    {
+                        if(targets_arr == map[i][j])
+                            {
+                                the_smallest.push(j+1);
+                                
+                            }
+                    }
+            }
+        
+        
+        if(the_smallest.length === 0)
+            {
+                return [-1];
+            }
+        else
+            {
+                return the_smallest;
+            }
+    }
+    
+    
+    for(let i = 0; i < targets_array.length; i++)
+        {
+
+            let value = 0;
+            for(let j = 0; j < targets_array[i].length; j++)
+                {
+                    value = value + Math.min(...find_target(targets_array[i][j]));
+                    if(Math.min(...find_target(targets_array[i][j])) === -1)
+                        {
+                            value = -1;
+                            break;
+                        }
+                }
+            result.push(value);
+        }
+
+
+    console.log(targets_array);
+    console.log(result);
+    var answer = result;
+    return answer;
+}
